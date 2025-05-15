11ec7a4da8f7284886c65729a6d9393491b74b10
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-05-15 20:30:43 +0530

    initial setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res, next) => {
+  res.send("Signup route");
+};
+
+export const login = async (req, res, next) => {
+  res.send("Login route");
+};
+
+export const logout = (req, res, next) => {
+  res.send("Logout route");
+};
