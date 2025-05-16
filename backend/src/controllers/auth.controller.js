4722ce38c35b62108323c49ff1d392d801806547
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-05-16 18:44:28 +0530

    Sing Up functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,65 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
-  res.send("Signup route");
+  const { email, password, fullName } = req.body;
+
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All Fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists, Please use a different email address",
+      });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1;
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    // TODO: CREATE THE USER IN STREAM AS WELL
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      httpOnly: true, // Prevents client-side JavaScript from accessing the cookie // prevent xss attack
+      sameSite: "strict", // Helps prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    });
+
+    res.status(201).json({
+      success: true,
+      user: newUser,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const login = async (req, res, next) => {
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,30 @@
 import express from "express";
 import dotenv from "dotenv"; // import "dotenv/config"; will also work
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json()); // Middleware to parse JSON bodies
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
+
+// connectDB()
+//   .then(() => {
+//     app.use("/api/auth", authRoutes);
+
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("Failed to connect to MongoDB. Server not started.", err);
+//   });
